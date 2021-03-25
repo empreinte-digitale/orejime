85a8e458d0505210eed6176a03fa67a890d4157e
Félix Girault <felix.girault@gmail.com>
2021-03-25 18:01:30 +0100

    Prettier formatting

--- src/components/apps.js
+++ src/components/apps.js
@@ -1,95 +1,98 @@
-import React from 'react'
+import React from 'react';
 import AppList from './app-list';
 import CategorizedAppList from './categorized-app-list';
 
 export default class Apps extends React.Component {
-    constructor(props, context){
-        super(props, context)
-        props.manager.watch(this)
-        this.state = {
-            consents : props.manager.consents
-        }
-    }
+	constructor(props, context) {
+		super(props, context);
+		props.manager.watch(this);
+		this.state = {
+			consents: props.manager.consents
+		};
+	}
 
-    componentWillUnmount(){
-        const {manager} = this.props
-        manager.unwatch(this)
-    }
+	componentWillUnmount() {
+		const {manager} = this.props;
+		manager.unwatch(this);
+	}
 
-    update(obj, type, data){
-        const {manager} = this.props
-        if (obj == manager && type == 'consents')
-            this.setState({consents : data})
-    }
+	update(obj, type, data) {
+		const {manager} = this.props;
+		if (obj == manager && type == 'consents') this.setState({consents: data});
+	}
 
-    render(){
-        const {config, t, ns, manager} = this.props
-        const {consents} = this.state
-        const {apps, categories} = config
+	render() {
+		const {config, t, ns, manager} = this.props;
+		const {consents} = this.state;
+		const {apps, categories} = config;
 
-        const toggleAll = (value) => {
-            apps.map((app)=>{
-                manager.updateConsent(app, value)
-            })
-        }
+		const toggleAll = (value) => {
+			apps.map((app) => {
+				manager.updateConsent(app, value);
+			});
+		};
 
-        const enableAll = () => toggleAll(true)
-        const disableAll = () => toggleAll(false)
+		const enableAll = () => toggleAll(true);
+		const disableAll = () => toggleAll(false);
 
-        const allDisabled = apps.filter((app) => {
-            return (app.required || false)
-                ? false
-                : consents[app.name]
-        }).length === 0
+		const allDisabled =
+			apps.filter((app) => {
+				return app.required || false ? false : consents[app.name];
+			}).length === 0;
 
-        const allEnabled = apps.filter((app) => {
-            return consents[app.name]
-        }).length === apps.length
+		const allEnabled =
+			apps.filter((app) => {
+				return consents[app.name];
+			}).length === apps.length;
 
-        const someOptional = apps.some((app) => !app.required);
+		const someOptional = apps.some((app) => !app.required);
 
-        return (
-            <div>
-                {someOptional ? (
-                    <div className={ns('AppToggles')}>
-                        <button
-                            type="button"
-                            className={ns('Button Button--info AppToggles-button AppToggles-enableAll')}
-                            disabled={allEnabled}
-                            onClick={enableAll}
-                        >
-                            {t(['acceptAll'])}
-                        </button>
-                        <button
-                            type="button"
-                            className={ns('Button Button--info AppToggles-button AppToggles-disableAll')}
-                            disabled={allDisabled}
-                            onClick={disableAll}
-                        >
-                            {t(['declineAll'])}
-                        </button>
-                    </div>
-                ) : null}
+		return (
+			<div>
+				{someOptional ? (
+					<div className={ns('AppToggles')}>
+						<button
+							type="button"
+							className={ns(
+								'Button Button--info AppToggles-button AppToggles-enableAll'
+							)}
+							disabled={allEnabled}
+							onClick={enableAll}
+						>
+							{t(['acceptAll'])}
+						</button>
+						<button
+							type="button"
+							className={ns(
+								'Button Button--info AppToggles-button AppToggles-disableAll'
+							)}
+							disabled={allDisabled}
+							onClick={disableAll}
+						>
+							{t(['declineAll'])}
+						</button>
+					</div>
+				) : null}
 
-                {categories ? (
-                    <CategorizedAppList
-                        t={t}
-                        ns={ns}
-                        categories={categories}
-                        apps={apps}
-                        consents={consents}
-                        onToggle={manager.updateConsent.bind(manager)}
-                    />
-                ) : (
-                    <AppList
-                        t={t}
-                        ns={ns}
-                        apps={apps}
-                        consents={consents}
-                        onToggle={manager.updateConsent.bind(manager)}
-                    />
-                )}
-            </div>
-        );
-    }
+				{categories ? (
+					<CategorizedAppList
+						t={t}
+						ns={ns}
+						categories={categories}
+						apps={apps}
+						consents={consents}
+						onToggle={manager.updateConsent.bind(manager)}
+					/>
+				) : (
+					<AppList
+						t={t}
+						ns={ns}
+						apps={apps}
+						consents={consents}
+						onToggle={manager.updateConsent.bind(manager)}
+					/>
+				)}
+			</div>
+		);
+	}
 }
--- src/utils/css.js
+++ src/utils/css.js
@@ -1,9 +1,9 @@
 export function createCssNamespace(namespace) {
 	return function ns(classNames) {
-		const splitClassNames = classNames.split(' ')
+		const splitClassNames = classNames.split(' ');
 		return splitClassNames
-			.filter(className => className.length > 0)
-			.map(className => `${namespace}-${className}`)
-			.join(' ')
-	}
+			.filter((className) => className.length > 0)
+			.map((className) => `${namespace}-${className}`)
+			.join(' ');
+	};
 }
