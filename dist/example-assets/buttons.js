5b2cf32d31715e861270d29db31c39723e509217
Félix Girault <felix.girault@gmail.com>
2022-05-11 15:01:42 +0200

    Added a way to preload the UI
    This might come in handy to improve the UX.
    For example, one could preload the UI when the user is about to click a
    button to open the modal.

--- dist/example-assets/buttons.js
+++ dist/example-assets/buttons.js
@@ -9,3 +9,12 @@ document.querySelectorAll('[data-orejime-reset]').forEach(function (trigger) {
 		window.orejime.manager.clearConsents();
 	});
 });
+
+document.querySelectorAll('[data-orejime-preload]').forEach(function (trigger) {
+	trigger.addEventListener('focus', function () {
+		window.orejime.preload();
+	});
+	trigger.addEventListener('mouseover', function () {
+		window.orejime.preload();
+	});
+});
