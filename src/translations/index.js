8497de88c41d6d675f2c0cc1d8b2cee9040a0d3d
Félix Girault <felix.girault@gmail.com>
2023-08-16 15:49:44 +0200

    Included missing translations
    Recently added translations weren't included in the build.

--- src/translations/index.js
+++ src/translations/index.js
@@ -4,23 +4,31 @@
 import ca from './ca.yml';
 import de from './de.yml';
 import en from './en.yml';
+import es from './es.yml';
+import et from './et.yml';
+import fi from './fi.yml';
 import fr from './fr.yml';
 import hu from './hu.yml';
 import it from './it.yml';
-import ro from './ro.yml';
 import nb from './nb.yml';
 import nl from './nl.yml';
 import oc from './oc.yml';
+import ro from './ro.yml';
+import sv from './sv.yml';
 
 export default {
 	ca,
 	de,
 	en,
+	es,
+	et,
+	fi,
 	fr,
 	hu,
 	it,
-	ro,
 	nb,
 	nl,
-	oc
+	oc,
+	ro,
+	sv
 };
