7a48276138ad8bff7316dcc8964295de68c7859f
Félix Girault <felix.girault@gmail.com>
2024-09-05 18:01:36 +0200

    Improved tests performance
    Using swc cuts down the execution time by a factor of 10.

--- src/core/EventEmitter.test.ts
+++ src/core/EventEmitter.test.ts
@@ -1,3 +1,4 @@
+import {jest} from '@jest/globals';
 import EventEmitter from './EventEmitter';
 
 describe('EventEmitter', () => {
--- src/core/Manager.test.ts
+++ src/core/Manager.test.ts
@@ -1,3 +1,4 @@
+import {jest} from '@jest/globals';
 import Manager from './Manager';
 import type {Purpose} from './types';
 
